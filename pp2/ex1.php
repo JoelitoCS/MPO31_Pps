e36bcf485db8cc1d6be5d6d4b4cfa580e0dd8b0b
JoelitoCS <159519823+JoelitoCS@users.noreply.github.com>
2025-09-19 16:28:56 +0000

    CanoJoel_Pp2

---
+++ pp2/ex1.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="ca">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejercicio 1: NUmeros parejos</title>
+    <style>
+
+        body {
+            background: #f9f9f9;
+            font-family: Arial, sans-serif;
+            margin: 0;
+        }
+
+        .contenedorBucle {
+            display: flex;
+            flex-wrap: wrap;
+            gap: 10px;
+            margin: 16px auto;
+            max-width: 900px;
+
+        }
+
+        .bucle {
+            background: #3498db;
+            color: #fff;
+            padding: 0.6rem 1.0rem;
+            border-radius: 10px;
+            font-size: 12px;
+            text-align: center;
+        }
+
+    </style>
+</head>
+<body>
+    <h1>Numeros parejos entre 50 y 500</h1>
+
+    <div class="contenedorBucle">
+        <?php
+        for ($i = 50; $i <= 500; $i = $i + 2) {
+            echo "<div class='bucle'>$i</div>";
+        }
+        ?>
+    </div>
+</body>
+</html>

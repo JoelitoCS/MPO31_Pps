c493702f8d0c14d8851a04d4ad41b5ea9b546541
JoelitoCS <159519823+JoelitoCS@users.noreply.github.com>
2025-10-13 13:27:42 +0000

    CanoJoel_Pp4

--- pp4/funciones.php
+++ pp4/funciones.php
@@ -16,6 +16,16 @@
         
         //cogemos la  disponibiliidad de nuestro array productos, y solo habran trues o falses, segúnn si es true o no le diremos que esta dispoible oo no y ya luego recorremos la tabla y ya fuera del bucle la cerramos
 
+        if ($producto['disponibilidad']) {
+
+            $colorRojo = "white";
+
+        } else {
+
+            $colorRojo = "red";
+            
+        }
+
         $disponibilidad = $producto['disponibilidad'];
 
         if($disponibilidad === true){
@@ -29,7 +39,7 @@
         };
 
         echo "
-            <tr>
+            <tr style='background-color:$colorRojo;'>
                 <td>{$producto['nombre']}</td>
                 <td>{$producto['precio']}</td>
                 <td>{$disponibilidad}</td>
@@ -55,7 +65,7 @@
 
         echo "<h3>Nombre: $nombre</h3>";
         echo "<h3>Teléfono: $telefono</h3>";
-        echo "<h3>Foto: $foto</h3>";
+        echo "<img src='$foto' alt='$foto'>";
 
 
     };
